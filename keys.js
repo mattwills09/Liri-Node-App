9f67e1d87e4600faf4e0c1e8599ac68afe2f450b
mattwills09 <mattwills09@gmail.com>
2019-04-18 14:13:35 -0400

    Added ignore file, keys and images

---
+++ keys.js
@@ -0,0 +1,3 @@
+//OMDb API Key: c2ad71b5
+//Spotify API Key: 4bd1c135cdc3491383776bbe7040c2fc
+//BANDS IN TOWN API QUERY: https://rest.bandsintown.com/artists/jennifer%20lopez/events?app_id=codingbootcamp
