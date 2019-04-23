8c1b4fbbbd50b1ca28af34a9096c94198d7dd7ee
mattwills09 <mattwills09@gmail.com>
2019-04-22 23:55:39 -0400

    Completed code for 'movie' and 'song'

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+var info = process.argv;
 var action = process.argv[2];
 var value = process.argv[3];
 
@@ -35,22 +36,22 @@ case "do-what-it-says":
 
 function concert() {
 
-    var nodeArgs = process.argv;
+    // var nodeArgs = process.argv;
     var artistName = "";
     
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            artistName = artistName + "+" + nodeArgs[i];
+    for (var i = 3; i < info.length; i++) {
+        if (i > 3 && i < info.length) {
+            artistName = artistName + "+" + info[i];
     }
     else {
-        artistName += nodeArgs[i];
+        artistName += info[i];
     }}
 
     console.log(artistName);
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
 
-    console.log(queryUrl);
+    // console.log(queryUrl);
 
     axios.get(queryUrl).then(
         function(response) {
@@ -59,7 +60,7 @@ function concert() {
             console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
 
             var date = response.data[0].datetime;
-            console.log(date);
+            // console.log(date);
             
             var showDate = moment(date).format("MM/DD/YYYY");
             console.log("Date: " + showDate);
@@ -69,19 +70,24 @@ function concert() {
 
 //--------------------------------------
 
-
 function song() {
 
-    var nodeSongs = process.argv;
+    // var nodeSongs = process.argv;
     var songName = "";
+
+    if (info.length > 3) {
     
-    for (var i = 3; i < nodeSongs.length; i++) {
-        if (i > 3 && i < nodeSongs.length) {
-        songName = songName + "+" + nodeSongs[i];
+    for (var i = 3; i < info.length; i++) {
+        if (i > 3 && i < info.length) {
+        songName = songName + "+" + info[i];
     }
     else {
-        songName += nodeSongs[i];
+        songName += info[i];
         }
+    }}
+
+    else {
+        songName = "The Sign";
     }
 
     console.log(songName);
@@ -90,7 +96,11 @@ function song() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-    console.log(data.tracks.items[0]);
+
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
     })
 };
 
@@ -101,13 +111,19 @@ function movie() {
     var nodeMovies = process.argv;
     var movieName = "";
     
+    if (nodeMovies.length > 3) {
+        
     for (var i = 3; i < nodeMovies.length; i++) {
         if (i > 3 && i < nodeMovies.length) {
         movieName = movieName + "+" + nodeMovies[i];
-    }
-    else {
+        }
+        else {
         movieName += nodeMovies[i];
         }
+    }}
+
+    else {
+        movieName = "Mr. Nobody";
     }
 
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
@@ -130,6 +146,16 @@ axios.get(queryUrl)
 
 //--------------------------------------
 
-// function doWhat() {
+function doWhat() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        console.log(data.split(","));
+        var commandArray = data.split(",");
+        console.log(commandArray[0]);
 
-// }
+
+    });
+}
